c71deede983ea322319d7418b940ef3be03fff96
Vivek Gupta <vivek.gupta8983@gmail.com>
2025-09-08 16:24:24 +0530

    Add additional server information to the database connection status display

--- server_info.php
+++ server_info.php
@@ -4,7 +4,11 @@ header('Content-Type: application/json');
 $server_info = [
     'server' => $_SERVER['SERVER_SOFTWARE'] ?? 'Unknown',
     'php_version' => phpversion(),
-    'server_time' => date('Y-m-d H:i:s')
+    'server_time' => date('Y-m-d H:i:s'),
+    'server_ip' => $_SERVER['SERVER_ADDR'] ?? gethostbyname(gethostname()),
+    'hostname' => gethostname(),
+    'client_ip' => $_SERVER['REMOTE_ADDR'] ?? 'Unknown',
+    'server_port' => $_SERVER['SERVER_PORT'] ?? 'Unknown'
 ];
 
 echo json_encode($server_info);
